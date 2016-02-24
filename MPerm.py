6d9588fb72bd24401f607bacd9c01078e03a6c85
Piper Chester <piperchester@gmail.com>
2016-02-24 16:24:33 -0500

    Create basic report.txt; add Permissions.py

--- MPerm.py
+++ MPerm.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-
 __author__ = 'piper'
 
 import glob
 import sys
 import subprocess
+
 import Harvest
+from Permissions import Permissions
 
 def read_manifest(project_root):
     """Analyze manifest to see what permissions to request."""
@@ -24,13 +25,35 @@ def read_manifest(project_root):
                 permission = permission_line[:permission_line.find('"')]
                 if permission not in permissions:
                     permissions.append(permission_line[:permission_line.find('"')])
+    return permissions
 
-    # Write add manifest to report
+
+def print_report(permissions, report_name):
+    """Prints permissions analysis report to report_name."""
     line_number = 1
-    with open("report.txt", "w") as report:
-        print('--- Permissions from Manifest ---', file=report)
-        for permission in permissions:
-            print('{:>4} {}'.format(line_number, permission.rstrip()), file=report)
+    with open(report_name, "w") as report:
+        print('--- MPermission Analysis Report ---\n\n', file=report)
+
+        print('--- System Permission Groups from Manifest ---', file=report)
+        # Distinguish between System and 3rd party
+        system_permissions = Permissions()
+        perms = system_permissions.get_permissions()
+        non_system_permissions = []
+        for permission_group, permission_list in perms.items():
+
+            # Bucket system permission to appropriate permission_group
+            print(permission_group.capitalize() + " Group:", file=report)
+            for new_permission in permissions:
+                if new_permission.rstrip() in permission_list:
+                    print('{:>4} {}'.format(line_number, new_permission.rstrip()), file=report)
+                    permissions.remove(new_permission)
+                    line_number += 1
+            print('\n', file=report)
+
+        line_number = 1
+        print('--- Non-Dangerous Permissions from Manifest ---', file=report)
+        for non_system_permission in permissions:
+            print('{:>4} {}'.format(line_number, non_system_permission), file=report)
             line_number += 1
         print('-' * 20, file=report)
 
@@ -61,8 +84,9 @@ def main():
         exit(1)
     elif len(arguments) >= 3 and len(arguments) < 5:
         source_path = arguments[1]
-        read_manifest(source_path)
-        if '-d' in arguments:
+        permissions = read_manifest(source_path)
+        print_report(permissions, "report.txt")
+        if '-h' in arguments:
             Harvest.search_project_root(source_path)
 
 
